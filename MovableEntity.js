e66e5ba21742284d3a7f20d046bfdf437bb336ff
wildum <helmutlbb@orange.fr>
2018-06-29 20:12:50 +0200

    movable entity inherited from vector and parent of unit and bulleté

---
+++ MovableEntity.js
@@ -0,0 +1,11 @@
+class MovableEntity extends Vector {
+
+    constructor(x, y) {
+        super(x, y);
+        this.dx = 0;
+        this.dy = 0;
+        this.speed = 0;
+        this.rotation = 0;
+    }
+
+}
